f0796b909d189aac81df4ae42c0d5c80b6c86b0b
Hugues Maignol <hugues@hmlb.fr>
2017-01-24 15:37:14 +0100

    AssertionFailed Exception extends Assertion library Exception

--- src/Exception/AssertionFailedException.php
+++ src/Exception/AssertionFailedException.php
@@ -4,11 +4,14 @@ declare (strict_types = 1);
 
 namespace HMLB\DDD\Exception;
 
+use Assert\AssertionFailedException as AssertionException;
+use Assert\InvalidArgumentException as BaseException;
+
 /**
  * A necessary specification failed.
  *
  * @author Hugues Maignol <hugues@hmlb.fr>
  */
-class AssertionFailedException extends Exception
+class AssertionFailedException extends BaseException implements AssertionException
 {
 }
